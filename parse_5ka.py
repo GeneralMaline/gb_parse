893d4f7c2a3373d4e84c5c9ac83e7b65f51bf632
GeneralMaline <mixspirov@gmail.com>
2021-01-17 19:19:26 +0900

    first commit

---
+++ parse_5ka.py
@@ -0,0 +1,81 @@
+import json
+import time
+from pathlib import Path
+import requests
+url = 'https://5ka.ru/special_offers/'
+
+# ?categories=&ordering=&page=2&price_promo__gte=&price_promo__lte=&records_per_page=12&search=&store=
+
+params = {
+    'categories': None,
+    'ordering': None,
+    'page': 2,
+    'price_promo__gte': None,
+    'records_per_page': 12,
+    'search': None,
+    'store': None
+}
+
+class ParseError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
+class parse5ka:
+    _headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
+    }
+    _params = {
+        'categories': None,
+        'ordering': None,
+        'page': None,
+        'price_promo__gte': None,
+        'records_per_page': 12,
+        'search': None,
+        'store': None
+    }
+
+    def __init__(self, start_url: str, result_path: Path):
+        self.start_url = start_url
+        self.result_path = result_path
+
+    @staticmethod
+    def __get_response(url: str, *args, **kwargs) -> requests.Response:
+        while True:
+            try:
+                response = requests.get(url, *args, **kwargs)
+                if response.status_code > 399:
+                    raise ParseError(response.status_code)
+                time.sleep(0.1)
+                return response
+            except (requests.RequestException, ParseError):
+                time.sleep(0.5)
+                continue
+
+    def run(self):
+        for product in self.parse(self.start_url):
+            file_path = self.result_path.joinpath(f'{product["id"]}.json')
+            self.save(product, file_path)
+
+    def parse(self, url) -> dict:
+        while url:
+            response = self.__get_response(
+                url, params = self._params, headers = self._headers
+            )
+            data = response.json()
+            url = data['next']
+            for product in data['results']:
+                yield product
+
+    @staticmethod
+    def save(data: dict, file_path:Path):
+        with file_path.open('w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False)
+            #file.write(json.dumps(data)) то же самое
+
+
+if __name__ == '__main__':
+    url = 'https://5ka.ru/api/v2/special_offers/'
+    result_path = Path(__file__).parent.joinpath('products_5ka')
+    parser = parse5ka(url, result_path)
+    parser.run()
